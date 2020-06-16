6412f252de96c3dc39158fb7311eed49759ab533
Shethzor <47732616+Shethzor@users.noreply.github.com>
2020-06-16 11:14:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -237,6 +237,27 @@ if (isEmptyOrNull(favicon)){
 }
 
 var head = document.querySelectorAll('head')[0];
+
+//Tab
+head.innerHTML += '<title id="title">wrw3r3rw</title>';
+head.innerHTML += '<link id="favicon" rel="icon" type="image/png" href="wrr3rwr"></link>';
+
+//Titles
+head.innerHTML += '<meta name="title" content="3rwrwr3">';
+head.innerHTML += '<meta property="og:title" content="wr3rwr3r">';
+head.innerHTML += '<meta property="twitter:title" content="3rw3rwr3">';
+
+//Descriptions
+head.innerHTML += '<meta name="description" content="w3rwrwr3r">';
+head.innerHTML += '<meta property="og:description" content="wr3wr3rw3r">';
+head.innerHTML += '<meta property="twitter:description" content="3wrw3rw3rwr">';
+
+//Images
+head.innerHTML += '<meta name="image" content="w3rw3r3rwr3">';
+head.innerHTML += '<meta property="og:image" content="w3rw3rw3r3r">';
+head.innerHTML += '<meta property="twitter:image" content="wr3wrwrw3r">';
+
+/*
 //Tab
 head.innerHTML += '<title id="title">' + title + '</title>';
 head.innerHTML += '<link id="favicon" rel="icon" type="image/png" href="' + favicon + '"></link>';
@@ -255,6 +276,7 @@ head.innerHTML += '<meta property="twitter:description" content="' + description
 head.innerHTML += '<meta name="image" content="' + favicon + '">';
 head.innerHTML += '<meta property="og:image" content="' + favicon + '">';
 head.innerHTML += '<meta property="twitter:image" content="' + favicon + '">';
+*/
 
 var isRedirect = false;
 for (var redirect in searchKey) {
